b08e157dca5d19513dd721fbb6a1fea6abc3f669
Vitaly Osipov <vital-nvl@mail.ru>
2022-12-16 21:11:34 +0300

    control

--- controller.py
+++ controller.py
@@ -1,28 +1,70 @@
+# Обработка введенных данных
+
 from console import console
 from add import add
 from read import read
+import csv
 
-run = console()
-if run == 1:
+
+def main():
+    run = console()
+    if run == 1:
+        one()
+    if run == 2:
+        two()
+    if run == 3:
+        three()
+    else:
+        print('Неправильный ввод!!!')
+        input('Чтобы продолжить, нажмите Enter')
+    main()
+
+
+def one():
     data = add()
     last_name, first_name, phone, description = data
-    print ('*'*20 + '  Данные заполнены  ' + '*'*20)
-    print (f'Фамилия: {last_name}')
+    print('*' * 20 + '  Данные заполнены  ' + '*' * 20)
+    print(f'Фамилия: {last_name}')
     print(f'Имя: {first_name}')
     print(f'Номер телефона: {phone}')
     print(f'Описание: {description}')
     print('*' * 20 + '  Данные успешно добавлены  ' + '*' * 20)
-    console()
-if run == 2:
+    input('Чтобы продолжить, нажмите Enter')
+    main()
+
+
+def two():
     data = read()
-if run == 3:
-    data = read()
-    print('*' * 20 + '  Данные для выгрузки в файл  ' + '*' * 20)
+    print('*' * 20 + '  Поиск абонента  ' + '*' * 20)
+    search = (input('Введите ключевой текст: ').title())
+
     for line in data:
-        print(line.strip())
+        if search in line:
+            print(f'Найдена запись:  {line}')
+    input('Чтобы продолжить, нажмите Enter')
+    main()
 
-    log = f'{last_name} {first_name} {phone} {description}\n'
-    with open('phonebook.txt', 'a', encoding='UTF-8') as file:
-        file.write(log)
-    #last_name, first_name, phone, description = data
 
+def three():
+    data = read()
+    form = int(input('Выберите формат файла данных (1. txt, 2. csv)'))
+    if form == 1:
+        print('*' * 20 + '  Данные для выгрузки в файл  ' + '*' * 20)
+        for line in data:
+            log = line.strip()
+            print(log)
+            with open('data_out.txt', 'a', encoding='UTF-8') as file:
+                file.write(log + '\n')
+
+    if form == 2:
+        print('*' * 20 + '  Данные для выгрузки в файл  ' + '*' * 20)
+        for line in data:
+            log = line.strip()
+            print(log)
+        with open('data_out.csv', 'w', newline='', encoding='UTF-8') as file:
+            writer = csv.writer(file)
+            writer.writerows([data])
+
+    print('*' * 20 + '  Выгрузка успешно завершена в файл data_out.txt  ' + '*' * 20)
+    input('Чтобы продолжить, нажмите Enter')
+    main()
