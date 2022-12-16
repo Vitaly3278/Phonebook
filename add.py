ecc80081e14fde2161327af7898cc6f185d360ff
Vitaly Osipov <vital-nvl@mail.ru>
2022-12-16 21:20:46 +0300

    added phone number

--- add.py
+++ add.py
@@ -1,5 +1,8 @@
 # Добавление абонента
 
+from write import add_data
+
+
 def add():
     print('*' * 20 + '  Добавление абонента  ' + '*' * 20)
     last_name = input('Введите фамилию: ')
@@ -7,4 +10,5 @@ def add():
     phone = input('Введите номер телефона: ')
     description = input('Описание (если нет, поставьте "-"): ')
     data = last_name.title(), first_name.title(), phone, description.title()
-    return (last_name.title(), first_name.title(), phone, description.title())
+    add_data(data)
+    return (data)
