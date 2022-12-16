45f4443f74dba07e315a02eabab2000839229b04
Vitaly Osipov <vital-nvl@mail.ru>
2022-12-16 21:12:49 +0300

    read file BD

--- read.py
+++ read.py
@@ -1,5 +1,5 @@
+# Чтение из файла базы
 def read():
-    #last_name, first_name, phone, description = data
     with open('phonebook.txt', 'r', encoding='UTF-8') as file:
         line = file.readlines()
     return line
